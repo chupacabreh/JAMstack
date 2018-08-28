c6d0245cea699d167822a3c117ab10cd5a6df6df
Matt Freitas <mattfreitas415@gmail.com>
2018-08-28 11:25:28 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>HOLA!</h1>
+    <p>Placeholder LOREM IPSUM etc etc.</p>
+    <p>PLZ PROVIDE ME WITH HACKATHON TICKETS. TY SO MUCH :)</p>
+    <p>-Matt Freitas</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
